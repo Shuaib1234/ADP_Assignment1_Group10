1c3f39d2e01c4bc20a8fc71467b875cd67d50c16
Manny Barnes <smannybarnes@gmail.com>
2021-03-29 22:48:51 +0200

    Barebones File Structure

---
+++ src/main/java/za/ac/cput/adp_assignment1_group10/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp_assignment1_group10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/za/ac/cput/adp_assignment1_group10/AppTest.java
@@ -0,0 +1,20 @@
+package za.ac.cput.adp_assignment1_group10;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
